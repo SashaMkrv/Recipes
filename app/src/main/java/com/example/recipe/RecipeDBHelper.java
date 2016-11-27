6f3db0e8351a65ddadfe33ac441dcf8f2d1d833a
ScreechNothing <sasha.makarava@gmail.com>
2016-11-27 07:17:06 -0500

    add db

---
+++ app/src/main/java/com/example/recipe/RecipeDBHelper.java
@@ -0,0 +1,20 @@
+package com.example.recipe;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by sasha on 2016-11-27.
+ */
+
+public class RecipeDBHelper extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
