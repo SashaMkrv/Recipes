a4e94975ee8454caa9ebac95e45b39ca123530b7
ScreechNothing <sasha.makarava@gmail.com>
2016-12-07 16:19:08 -0500

    Kind of searches booleans now. ordering is messed up though.

--- app/src/main/java/com/example/recipe/MainActivity.java
+++ app/src/main/java/com/example/recipe/MainActivity.java
@@ -16,9 +16,16 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 import android.os.Bundle;
 import android.app.Activity;
@@ -112,28 +119,95 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public long[] calculateRelevance(){
+        long[] ids;
         RecipeDBHelper db = new RecipeDBHelper(this);
         TextView ingredients = (TextView) findViewById(R.id.ingredientSearch);
         if(ingredients.getText().toString().equals("")) {
             return db.getAll();
         }
-        return db.searchIngredient(ingredients.getText().toString());
-        /**Scanner scan = new Scanner(ingredients.getText().toString());
-        ArrayList<long[]> ids = new ArrayList<long[]>();
-        ArrayList<String> terms = new ArrayList<String>();
-        String term;
-        HashMap<Long, Integer> vals = new HashMap<Long, Integer>();
-        ArrayList<String> dontTerm = new ArrayList<String>();
-        while(scan.hasNext()) {
+        else{
+            ArrayList<Long> idGroups = new ArrayList<Long>();
+            Set<Long> currentGroup = new HashSet<Long>();
+            Scanner scan = new Scanner(ingredients.getText().toString());
+            /**ArrayList<long[]> ids = new ArrayList<long[]>();
+             ArrayList<String> terms = new ArrayList<String>();
+             String term;
+             HashMap<Long, Integer> vals = new HashMap<Long, Integer>();
+             ArrayList<String> dontTerm = new ArrayList<String>();
+             */
+            String term;
+            while(scan.hasNext()) {
                 switch(term = scan.next()) {
                     case "AND":
-                    //case "OR":
-                    case "NOT": dontTerm.add(scan.next());
+                        idGroups.addAll(currentGroup);
+                        currentGroup.clear();
+                        if (scan.hasNext()) term = scan.next();
+                    case "OR":
+                        if (scan.hasNext()) term = scan.next();
+                    case "NOT":
+                        if (scan.hasNext()) currentGroup.addAll(getSearchIDs(false, scan.next(), db));
+                        break;
+                    default:
+                        currentGroup.addAll(getSearchIDs(true, term, db));
+                }
 
-                    default: terms.add(term);
-                        ids.add(db.searchIngredient(term));
+                if(!scan.hasNext()){
+                    idGroups.addAll(currentGroup);
                 }
             }
-        }*/
+
+            //EditText catOrType = (EditText)findViewById(R.id.typeSearch);
+
+
+            //what an overly complicated way to get a search ranking
+            HashMap<Long, Integer> rank = new HashMap<Long, Integer>();
+            Long id;
+            Log.i("wwwhhhaaaat", ""+ idGroups.get(0).longValue());
+            Iterator<Long> iter = idGroups.iterator();
+            while(iter.hasNext()) {
+                id = iter.next().longValue();
+                Log.i("what even", "id " + idGroups);
+                if (rank.containsKey(id)) rank.put(id, rank.get(id) + 1);
+                else rank.put(id, 1);
+            }
+            Set<Long> allIds = new HashSet<>();
+            allIds.addAll(idGroups);
+            List<Long> allIdsL = new ArrayList<Long>(allIds);
+            //to avoid destroying the iterator
+            iter = allIds.iterator();
+            Long check;
+            while (iter.hasNext()){
+                check = iter.next();
+                Log.i("so ok, how about", "" + check);
+                if (rank.get(check) < 1){
+                    allIdsL.remove(check);
+                }
+                //remove all results with less than 1 match
+            }
+            Collections.sort(allIdsL, Collections.reverseOrder(new IdCompare(rank)));
+            long[] finalIds = new long[allIdsL.size()];
+            for (int i = 0; i < finalIds.length; i++){
+                finalIds[i] = allIdsL.get(i);
+            }
+            return finalIds;
+        }
+    }
+    private ArrayList<Long> getSearchIDs(boolean not, String term, RecipeDBHelper db){
+        long[] tmp = db.searchIngredient(term);
+        ArrayList<Long> returnValues = new ArrayList<Long>(tmp.length);
+        for(long l: tmp){
+            returnValues.add(l);
+        }
+        return returnValues;
+    }
+
+    private class IdCompare implements Comparator<Long> {
+        HashMap<Long, Integer> hm;
+        public IdCompare(HashMap h){
+            hm = h;
+        }
+        public int compare(Long l1, Long l2){
+            return hm.get(l1) - hm.get(l2);
+        }
     }
 }
